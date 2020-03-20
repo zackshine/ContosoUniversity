1ebda658bd4eec7d92b689dd717b8e62f2fa631e
zackshine <zackshine@163.com>
2020-03-20 17:50:43 +0800

    complex model

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -11,14 +11,23 @@ namespace ContosoUniversity.DAL
         {
         }
 
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             //阻止复数表名称
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Course>()
+                         .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                         .Map(t => t.MapLeftKey("CourseID")
+                             .MapRightKey("InstructorID")
+                             .ToTable("CourseInstructor"));
         }
     }
 }
