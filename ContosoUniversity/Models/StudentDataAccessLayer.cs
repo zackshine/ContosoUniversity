fd184d339d641820b9da5566b475913e71c6be7a
zackshine <zackshine@163.com>
2020-04-07 16:32:03 +0800

    store procedure

---
+++ ContosoUniversity/Models/StudentDataAccessLayer.cs
@@ -0,0 +1,105 @@
+﻿using ContosoUniversity.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class StudentDataAccessLayer
+    {
+        string connectionString = ConnectionString.CName;
+
+        public IEnumerable<Student> GetAllStudent()
+        {
+            List<Student> lstStudent = new List<Student>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Student student = new Student();
+                    student.ID = Convert.ToInt32(rdr["ID"]);
+                    student.FirstMidName = rdr["FirstMidName"].ToString();
+                    student.LastName = rdr["LastName"].ToString();
+                   
+
+                    lstStudent.Add(student);
+                }
+                con.Close();
+            }
+            return lstStudent;
+        }
+        public void AddStudent(Student student)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@FirstMidName", student.FirstMidName);
+                cmd.Parameters.AddWithValue("@LastName", student.LastName);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spUpdateStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id", student.ID);
+                cmd.Parameters.AddWithValue("@FirstMidName", student.FirstMidName);
+                cmd.Parameters.AddWithValue("@LastName", student.LastName);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        public Student GetStudentData(int? id)
+        {
+            Student student = new Student();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * FROM Student WHERE Id= " + id;
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    student.ID = Convert.ToInt32(rdr["Id"]);
+                    student.FirstMidName = rdr["FirstMidName"].ToString();
+                    student.LastName = rdr["LastName"].ToString();
+                    
+                }
+            }
+            return student;
+        }
+
+        public void DeleteStudent(int? id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
---
+++ ContosoUniversity/Utility/ConnectionString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Utility
+{
+    public static class ConnectionString
+    {
+        private static string cName = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=ContosoUniversity2;Integrated Security=SSPI;";
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
